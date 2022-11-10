9070fd78d05d974aa1355180b9bbd355c65cd49e
MarcoMadeddu <marcomade99@gmail.com>
2022-11-10 17:51:22 +0100

    First commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import Root from './routes/root';
+import ErrorPage from './routes/error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact/>
+      },
+    ]
+  },
+ 
+
+])
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
